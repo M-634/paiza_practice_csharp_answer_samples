3c5bfbf3c85c3db8deec979fea321fabe360b37e
M-634 <miyanagateru634@gmail.com>
2025-07-26 15:16:39 +0900

    add graph_dfs_trail_one step3 answer

---
+++ graph_dfs_problems/graph_dfs__trail_one/step3.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    // vertex number, adjacencyList
+    static Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+    // 出力パス
+    static List<int> outPutPath = new List<int>();
+
+    static void Main()
+    {
+        int n, s, t;
+        var input = ReadIntsFromConsole().ToArray();
+        n = input[0];
+        s = input[1];
+        t = input[2];
+
+        // グラフ作成
+        for (int i = 1; i <= n; i++)
+        {
+            SkipLine();//不要な入力をスキップ
+            var adjacencyList = ReadIntsFromConsole().ToList();
+            graph.Add(i, adjacencyList);
+        }
+
+        // DFS開始
+        var trail = new List<int>();
+        var useEdges = new HashSet<(int, int)>();
+        DfsRecursive(s, s, trail, useEdges, t);
+        Output(outPutPath);
+    }
+
+
+    static void DfsRecursive(int s, int current, List<int> trail, HashSet<(int, int)> useEdges, int t)
+    {
+        trail.Add(current);
+
+        if (current == t)
+        {
+            //最長トレイル更新
+            if (outPutPath.Count < trail.Count)
+            {
+                outPutPath = new List<int>(trail);
+            }
+        }
+
+        foreach (var neighbor in graph[current])
+        {
+            //経路チェック:完全無向グラフなので例えば（１，２）（２，１）経路を同じ扱いにする
+            var edge = (Math.Min(current, neighbor), Math.Max(current, neighbor));
+            if (useEdges.Add(edge))
+            {
+                DfsRecursive(s, neighbor, trail, useEdges, t);
+                useEdges.Remove(edge);
+            }
+        }
+
+        trail.RemoveAt(trail.Count - 1);
+    }
+
+    static void Output<T>(IEnumerable<T> list)
+    {
+        Console.WriteLine(string.Join(' ', list));
+    }
+
+     static void SkipLine()
+    {
+        Console.ReadLine();
+    }
+
+    static IEnumerable<int> ReadIntsFromConsole()
+    {
+        return Console.ReadLine().Split(' ').Select(int.Parse);
+    }
+}
